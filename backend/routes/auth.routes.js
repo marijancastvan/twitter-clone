56ac7a5ce0ff0aeba7e9c610d926ef59b9cb615a
Marijan <castvan.marian@gmail.com>
2025-04-26 23:40:28 +0200

    1 - auth controllers added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { logout, signup, login } from "../contollers/auth.controller.js";
+import { logout, signup, login, getMe } from "../contollers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
+router.get("/me", protectRoute, getMe);
 router.post("/signup", signup);
-
 router.post("/login", login);
-
 router.post("/logout", logout);
 
 
